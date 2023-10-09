6b02313523d6068b2edc73b787664b5a93edd3fb
Stefan Ylimäki <stefan.ylimaki@hillagroup.fi>
2023-10-09 15:35:50 +0300

    themes and navbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
-export default function Home() {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
-}
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
