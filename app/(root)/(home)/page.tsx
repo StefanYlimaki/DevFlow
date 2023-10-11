dbf20fbc8f79869c8b742139b01861041af62f88
Stefan Ylimäki <stefan.ylimaki@hillagroup.fi>
2023-10-11 15:03:43 +0300

    Home page changes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,66 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearch from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
+
+const questions = [
+  {
+    _id: 1,
+    title: "How to Handle Your Program's Emotional Breakdowns",
+    tags: [
+      { _id: 101, name: "Programming" },
+      { _id: 102, name: "Emotions" },
+    ],
+    author: {
+      _id: 201,
+      name: "Dr. Codey Feelgood",
+      picture: "/assets/icons/avatar.svg",
+    },
+    upvotes: 42,
+    views: 987,
+    answers: [],
+    createdAt: new Date("2023-10-10T17:30:00"),
+  },
+  {
+    _id: 2,
+    title: "The Secret Life of Debugging Ducks",
+    tags: [
+      { _id: 103, name: "Debugging" },
+      { _id: 104, name: "Duckology" },
+    ],
+    author: {
+      _id: 202,
+      name: "Quacky Debugger",
+      picture: "/assets/icons/avatar.svg",
+    },
+    upvotes: 58,
+    views: 1234,
+    answers: [],
+    createdAt: new Date("2023-10-09T12:15:00"),
+  },
+  {
+    _id: 3,
+    title: "The Great API Bake-Off: Where Code Meets Cake",
+    tags: [
+      { _id: 105, name: "API" },
+      { _id: 106, name: "Baking" },
+    ],
+    author: {
+      _id: 203,
+      name: "Baker Dev",
+      picture: "/assets/icons/avatar.svg",
+    },
+    upvotes: 35,
+    views: 2023,
+    answers: [],
+    createdAt: new Date("2023-10-08T09:45:00"),
+  },
+];
 
 const Home = () => {
   return (
@@ -35,6 +91,35 @@ const Home = () => {
         />
 
         <HomeFilters />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <div>
+            <NoResult
+              title="There are no questions to show"
+              description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+        discussion. our query could be the next big thing others learn from. Get
+        involved! 💡"
+              link="/ask-question"
+              linkTitle="Ask a Question"
+            />
+          </div>
+        )}
       </div>
     </>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,35 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimeStamp = (createdAt: Date): string => {
+  const now = new Date();
+  const diff = now.getTime() - createdAt.getTime();
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (days > 0) {
+    return `${days} day${days > 1 ? "s" : ""} ago`;
+  } else if (hours > 0) {
+    return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+  } else {
+    return `${seconds} second${seconds > 1 ? "s" : ""} ago`;
+  }
+};
+
+export const formatNumber = (num: number): string => {
+  if (num >= 1000000) {
+    return `${(num / 1000000).toFixed(1)}M`;
+  } else if (num >= 1000) {
+    return `${(num / 1000).toFixed(1)}K`;
+  } else {
+    return num.toString();
+  }
+};
